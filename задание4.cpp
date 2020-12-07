31ddb50c09243b5d25747e357af714360a34ef75
kirillcircle <73948408+kirillcircle@users.noreply.github.com>
2020-12-07 23:41:31 +0300

    Add files via upload

---
+++ задание4.cpp
@@ -0,0 +1,24 @@
+
+int i = 1;
+
+int j = 1;
+
+do {
+
+	do {
+
+		std::cout << i << " * " << j << " = " << i * j << std::endl;
+
+		j++;
+
+	} while (j < 10);
+
+	i++;
+
+	j = 1;
+
+} while (i < 10);
+
+return 0;
+
+}
